19de41917b5c18b4be214aa0f330bf5fee66e241
Joshua DeLong <48997663+jjdelong@users.noreply.github.com>
2019-11-07 10:13:23 -0600

    Create functions.js

---
+++ sapphire-files/js/functions.js
@@ -0,0 +1,73 @@
+
+/*
+    *** SYNTAX: window.open(URL, name, specs, replace)
+
+    *** Open about:blank page in new window/tab ***
+    var myWindow = window.open("", "", "width=200,height=100");
+
+    *** Replace current window with a new window ***
+    var myWindow = window.open("", "_self");
+    myWindow.document.write("<p>I replaced the current window.</p>");
+
+    *** Open new window called "MsgeWindow", and write some text ***
+    var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+    myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
+    var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+    myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
+
+    *** Open a new window and control appearance  ***
+    window.open("https://www.w3schools.com", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+
+    *** Open multiple tabs ***
+    window.open("http://www.google.com/", "width=);
+    window.open("https://www.w3schools.com/");
+
+    *** Open new window, use close() to close the new window ***
+    function openWin() {
+    myWindow = window.open("", "myWindow", "width=200,height=100");   // Opens a new window
+
+    function closeWin() {
+    myWindow.close();   // Closes the new window
+*/
+
+// Scripts for opening game links
+var gameOne = ;
+var gameTwo = ;
+var gameThree = ;
+var gameFour = ;
+var gameFive = ;
+
+/* Game one open/close */
+function openGameOne():         // ADD game url
+    gameOne.open("/* URL Placeholder */", "_self")
+
+function closeGameOne():
+    gameOne.close();
+
+/* Game two open/close */
+function openGameTwo():         // ADD game url
+    gameTwo.open("/* URL Placeholder */", "_self")
+
+function closeGameTwo():
+    gameTwo.close();
+
+/* Game threeopen/close */
+function openGameThree():         // ADD game url
+    gameThree.open("/* URL Placeholder */", "_self")
+
+function closeGameThree():
+    gameThree.close();
+
+/* Game four open/close */
+function openGameFour():         // ADD game url
+    gameFour.open("/* URL Placeholder */", "_self")
+
+function closeGameFour():
+    gameFour.close();
+
+/* Game five open/close */
+function openGameFive():         // ADD game url
+    gameFive.open("/* URL Placeholder */", "_self")
+
+function closeGameFive():
+    gameFive.close();
